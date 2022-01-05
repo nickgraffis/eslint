71c8ff775ff1dbe62589eba73669b1f7cf44c017
Nick Graffis <nicholasgraffis@gmail.com>
2022-01-05 13:46:24 -0800

    fixed pathing

--- scripts/basic.js
+++ scripts/basic.js
@@ -1,4 +1,4 @@
-const VERSION = require('..packages/basic/package.json').version;
+const VERSION = require('../packages/basic/package.json').version;
 const run = require('./index.js');
 
 module.exports = function() { run(`@nickgraffis/eslint-basic`, VERSION) };
--- scripts/typescript.js
+++ scripts/typescript.js
@@ -1,4 +1,4 @@
-const VERSION = require('..packages/typescript/package.json').version;
+const VERSION = require('../packages/typescript/package.json').version;
 const run = require('./index.js');
 
 module.exports = function() { run(`@nickgraffis/eslint-ts`, VERSION) };
